e82b18493ec2912b56406c5d6d2aa2558297ff2a
doffaarinalhaq <adoffa@gmail.com>
2022-09-29 01:32:08 +0700

    Login & Register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register'
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route exact path="/" element={<Login />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Register.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/Register.css';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+const Login = () => {
+  return (
+    <Container className="contlog">
+      <Row className="rowreg">
+        <Col lg={{ span: 7, offset: 0 }}>
+          <div href="#">
+            <img src="https://images.unsplash.com/photo-1530021232320-687d8e3dba54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" width="600" height="625" alt="logo" />
+          </div>
+        </Col>
+        <Col lg={{ span: 5, offset: 0 }} className="colreg2">
+          <h1>Register to Happy App!</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Select className="mb-3" aria-label="Default select example">
+              <option>Gender</option>
+              <option value="admin">Male</option>
+              <option value="default">Female</option>
+            </Form.Select>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form>
+          <Link to="/homepage"><Button className="logbtn">SIGN UP</Button></Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
